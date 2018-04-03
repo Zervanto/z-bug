c03f7c8e3ce78204a8a64d0512d00939a182c2da
JoJin <1019335509@qq.com>
2018-04-03 20:01:11 +0800

    mouting

---
+++ demoreact.js
@@ -0,0 +1,27 @@
+function creatrElement(type, config, children) {
+    const props = Object.assign({},config);
+    const childrenLength = [].slice.call(arguments).length - 2;//arguments对象的数组提出来转化为数组
+    if(childrenLength > 1){
+        props.children = [].slice.call(arguments,2);
+    }else if (childrenLength === 1){
+        props.children = children;
+    }
+    return {
+        type,
+        props
+    }
+}
+
+function instantiateComponent(element){
+    let componentInstance;
+    if(typeof element.type === 'function'){
+        componentInstance = new element.type(element.props);
+        componentInstance._construct(element);
+    }else if (typeof element === 'string' || typeof element === 'number'){
+        componentInstance = new DOMComponent({
+            type:'span',
+            props:{children:element}
+        });
+    }
+    return componentInstance;
+}
